53093f043615b8ad6fbcbfae2b9e5df52c3f72cb
Medric Riley <medricr@gmail.com>
2020-05-11 11:14:53 -0700

    formatting changes to bio component

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import {Jumbotron, Container, Row, Col} from 'reactstrap';
+
 import ProjectCard from '../ProjectCard';
 
 class Bio extends React.Component {
 
 	render() {
 		return (
+
+			<Jumbotron>
+				
+			</Jumbotron>
 			
-				<ProjectCard 
-					projectTitle="test" 
-					projectSubtitle="subtitleTest" 
-					projectDescription="this is where the description of the project will go " 
-				/>
+				// <ProjectCard 
+				// 	projectTitle="test" 
+				// 	projectSubtitle="subtitleTest" 
+				// 	projectDescription="this is where the description of the project will go " 
+				// />
 			
 		)
 	}
